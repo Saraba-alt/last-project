72ede2a1fe5afbad70bec452930d1e63367a3aad
Barsbold <saraba.q3q@gmail.com>
2020-12-30 16:33:54 +0800

    state bolon search controller nemlee

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
-require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+/**?
+ * Web app's state
+ * - Хайлтийн query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайклсан жорууд
+ * - Захиалж байгаа жорийн нарлаганууд
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+  // Web-с хайлтийн түлхүүр үгийг авна
+  const query = "pizza";
+
+  if (query) {
+    // Шинээр хайлтийн объектийг үүсгэж өгнө
+    state.search = new Search(query);
+    // Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ
+    // Хайлтийг гүйцэтгэнэ
+    await state.search.doSearch();
+    // Хайлтийн үр дүнг дэлгэцэнд гаргана
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
